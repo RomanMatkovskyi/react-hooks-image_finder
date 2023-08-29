a4d815cdb38e98d4706385c2f6ca82e046cbb294
RomanMatkovskyi <121770356+RomanMatkovskyi@users.noreply.github.com>
2023-08-29 21:38:52 +0200

    fix UseEffect(PopUp)

--- src/components/PopUp/PopUp.jsx
+++ src/components/PopUp/PopUp.jsx
@@ -2,28 +2,35 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const PopUp = ({ modalOpen, setModalOpen, tempingSrc }) => {
+  const onClose = () => {
+    setModalOpen(false);
+  };
+
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      setModalOpen(false);
+      onClose();
+    }
+  };
+
   useEffect(() => {
-    window.addEventListener('keydown', event => {
-      if (event.key === 'Escape' && modalOpen) {
-        setModalOpen(false);
+    window.addEventListener('keydown', handleKeyDown);
+
+    function handleKeyDown(e) {
+      if (e.code === 'Escape') {
+        onClose();
       }
-    });
+    }
 
     return () => {
-      window.removeEventListener('keydown', event => {
-        if (event.key === 'Escape' && modalOpen) {
-          setModalOpen(false);
-        }
-      });
+      window.removeEventListener('keydown', handleKeyDown);
     };
-  }, []);
+  }, [onClose]);
 
   return (
     <div
       className={modalOpen ? 'Overlay' : 'Hidden'}
-      onClick={() => {
-        setModalOpen(false);
-      }}
+      onClick={handleBackdropClick}
     >
       <div className={modalOpen ? 'Modal' : 'Hidden'}>
         <img src={tempingSrc} alt="" className="modalImage" />
